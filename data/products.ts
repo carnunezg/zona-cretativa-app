54c3cf6867dcc9c472c183b4c8f040049967b822
Carlos Nuñez <tu-email@gmail.com>
2026-03-06 07:12:14 -0300

    update: se agrega un nuevo objeto en el arreglo

--- data/products.ts
+++ data/products.ts
@@ -3,6 +3,17 @@ import { Product } from "@/types/product";
 export const products: Product[] = [
   {
     id: 1,
+    name: " Castillo Inflable Saltarín",
+    price: "25.000",
+    dimensions: "2.70 mt largo x 2.40 mt ancho x 1.95 mt alto",
+    image: "/images/castilloInflable.webp",
+    duration: "4 horas",
+    age: "0 a 5 años",
+    description:
+      "Colorido inflable diseñado especialmente para los más pequeños. Perfecto para cumpleaños y celebraciones infantiles, ofreciendo un espacio seguro donde los niños pueden saltar, jugar y disfrutar durante horas.",
+  },
+  {
+    id: 2,
     name: "Castillo Inflable",
     price: "25.000",
     dimensions: "4.60mt Largo, x 2.20mt Ancho, x 2.19mt Alto",
@@ -13,7 +24,7 @@ export const products: Product[] = [
       "Inflable amplio y seguro, ideal para que los niños salten, jueguen y se diviertan durante toda la celebración.",
   },
   {
-    id: 2,
+    id: 3,
     name: "Piscina de Pelotas",
     price: "25.000",
     dimensions: "1.60mt Largo x 1.60mt Ancho x 40cm Alto",
@@ -24,7 +35,7 @@ export const products: Product[] = [
       "Espacio de juego sensorial con pelotas de colores, perfecto para estimular la exploración y el juego libre.",
   },
   {
-    id: 3,
+    id: 4,
     name: "Resbalín",
     price: "5.000",
     dimensions: "104cm Largo x 36cm Ancho x 75cm Alto",
@@ -35,7 +46,7 @@ export const products: Product[] = [
       "Resbalín infantil compacto y estable, ideal para que los niños disfruten de deslizamientos seguros.",
   },
   {
-    id: 4,
+    id: 5,
     name: "Pony Saltarín",
     price: "3.000 C/U",
     dimensions: "60cm Largo x 25cm Ancho x 50cm Alto",
